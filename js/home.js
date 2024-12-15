428d7dc9fa7835d7fe7115b66ee24cf10ed5f417
MD Shoikot <mdshoikot202123@gmail.com>
2024-12-15 19:41:55 +0600

    add money button complete

--- js/home.js
+++ js/home.js
@@ -13,7 +13,27 @@ document.getElementById('btn-add-money')
 
 
          //get the pin number provided
-         const pinInput = document.getElementById('input-pin-number').value;
-         console.log(pinInput);
+         const pinNumberInput = document.getElementById('input-pin-number').value;
+         console.log(pinNumberInput); 
+
+         //verify the pin number
+         if(pinNumberInput == '1234'){
+            
+            //step-4: get the current balance
+            const balance = document.getElementById('account-balance').innerText;
+            console.log(balance);
+
+            //step-5: add addMoneyInput with balance
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const addBalanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + addBalanceNumber;
+            console.log(newBalance);
+
+            //step-6: update the balance in the UI/DOM
+            document.getElementById('account-balance').innerText = newBalance;
+
+         }else{
+            alert('failed to adding money')
+         }
 
 })
